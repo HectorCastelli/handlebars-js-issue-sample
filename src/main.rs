adde92256cf738533003b2ecbf9002cb6826930a
Hector Castelli Zacharias <hector.zacharias@gmail.com>
2023-07-04 23:02:10 +0200

    implement example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+use handlebars::Handlebars;
+use serde_json::json;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut handlebars = Handlebars::new();
+
+    let data = json!({
+        "data": {
+            "name": "John",
+            "related": {
+                "mary": {
+                    "relationship": "Siblings"
+                }
+            }
+        },
+        "other": {
+            "contents": {
+                "john": {
+                    "name": "John",
+                    "related": {
+                        "mary": {
+                            "relationship": "Siblings"
+                        }
+                    }
+                },
+                "mary": {
+                    "name": "Mary"
+                }
+            }
+        }
+    });
+
+    let template = r#"
+        # {{data.name}}
+
+        {{#each data.related as |related|}}
+        {{#with (lookup ../other.contents @key) as | other_related |}}
+        ## [{{other_related.name}}](/{{@key}})
+        {{related.relationship}}
+        {{/with}}
+        {{/each}}
+        "#;
+
+    println!(
+        "{}",
+        handlebars.render_template(template, &data)?);
+
+    Ok(())
 }
